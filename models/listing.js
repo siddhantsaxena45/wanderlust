f34b965a170e3f3c9bd34b0044426b2a7fba2ffc
siddhant <siddhant.saxena.2004@gmail.com>
2025-01-14 09:03:49 +0530

    add;

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema=mongoose.Schema;
+const Review =require("./review");
 const listingSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String , required: true},
@@ -19,6 +20,10 @@ const listingSchema = new Schema({
     country: { type: String,required: true },
     reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }]
 });
-
+listingSchema.post("findOneAndDelete",async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id:{$in: listing.reviews}});
+    }
+});
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
