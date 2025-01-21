ae9d2c356663da2d3621d9ab5bed1b6ad79b6927
siddhant <siddhant.saxena.2004@gmail.com>
2025-01-21 19:17:20 +0530

    signup

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,24 @@
 
 const express = require("express");
 const router = express.Router();
+const user = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync");
 
-router.get("/signup",(req,res)=>{
+router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 })
+router.post("/signup", wrapAsync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new user({ email, username });
+        const registeredUser = await user.register(newUser, password);
+        req.flash("success", "Signed Up successfully");
+        console.log(registeredUser);
+        res.redirect("/listings");
+    } catch (e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+
+}));
 module.exports = router;
