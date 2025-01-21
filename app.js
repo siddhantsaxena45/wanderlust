cced399cf23a100084be8db0af18397196b00a20
siddhant <siddhant.saxena.2004@gmail.com>
2025-01-21 18:49:03 +0530

    signup

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const methodOverride = require('method-override');
 const ejsmate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
 const ExpressError = require("./utils/expressError");
-const listings = require("./routes/listings");
-const Reviews = require("./routes/reviews");
+const listingRouter = require("./routes/listings");
+const reviewRouter = require("./routes/reviews");
+const userRouter = require("./routes/user");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User=require("./models/user");
 
 const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 async function main() {
@@ -44,14 +48,21 @@ app.get("/", wrapAsync(async (req, res) => {
 
 app.use(session(sessionOptions));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
     next();
 })
 
-app.use("/listings", listings);
-app.use("/listings/:id/review", Reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/review", reviewRouter);
+app.use("/",userRouter );
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "page not found!"));
